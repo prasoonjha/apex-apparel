ebf7afb16ca171a84cf8965c7112ce083f2e44d7
Prasoon Jha <prasoonjha69@gmail.com>
2020-05-20 20:01:13 +0530

    added cart items grouping functionality

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -12,12 +12,7 @@ const CollectionPreview = ({ title, items }) => {
         {items
           .filter((item, index) => index < 4)
           .map((item) => (
-            <CollectionItem
-              key={item.id}
-              name={item.name}
-              price={item.price}
-              imageUrl={item.imageUrl}
-            />
+            <CollectionItem key={item.id} item={item} />
           ))}
       </div>
     </div>
