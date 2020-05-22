5e8401f0e2ceeee65354a9bf282c8d292002756b
Prasoon Jha <prasoonjha69@gmail.com>
2020-05-23 01:09:43 +0530

    added cart total selector; fixed dropdown toggle on redirect to checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,12 +3,13 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
 import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import CartItem from "../cart-item/cart-item.component";
 import CustomButton from "../custom-button/custom-button.component";
 
 import "./cart-dropdown.styles.scss";
 
-const Cart = ({ cartItems, history }) => {
+const Cart = ({ cartItems, history, dispatch }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -20,7 +21,12 @@ const Cart = ({ cartItems, history }) => {
           ))
         )}
       </div>
-      <CustomButton onClick={() => history.push("/checkout")}>
+      <CustomButton
+        onClick={() => {
+          history.push("/checkout");
+          dispatch(toggleCartHidden());
+        }}
+      >
         GO TO CHECKOUT
       </CustomButton>
     </div>
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,8 +1,44 @@
 import React from "react";
+import { connect } from "react-redux";
 
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../redux/cart/cart.selectors";
 import "./checkout.styles.scss";
+import CartItem from "../../components/cart-item/cart-item.component";
 
-const CheckoutPage = () => {
-  return <div>CheckoutPage</div>;
+const CheckoutPage = ({ cartItems, cartTotal }) => {
+  return (
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map((cartItem) => cartItem.name)}
+      <div className="total">
+        <span>TOTAL: ${cartTotal}</span>
+      </div>
+    </div>
+  );
 };
-export default CheckoutPage;
+
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
+  cartTotal: selectCartTotal(state),
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
